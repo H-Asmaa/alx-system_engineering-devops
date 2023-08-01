3d336ffa574325582a3e793182954c359326d5fe
H-Asmaa <asmaehadar32@gmail.com>
2023-08-01 21:34:22 +0100

    Committing still in factory

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+sender = ARGV[0].scan(/(?<=from:)\s*([^\]]*?)(?=\])/)
+sender = ARGV[0].scan(/(?<=from:)\s*([^\]]*?)(?=\])/)
+sender = ARGV[0].scan(/(?<=from:)\s*([^\]]*?)(?=\])/)
+puts sender.join
