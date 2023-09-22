a9bf6ef6bd78221003de52deda637f21b93480d3
H-Asmaa <asmaehadar32@gmail.com>
2023-09-22 19:09:58 +0100

    Committing a manifest that creates a file with a certain permissions and users

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Creation of a file with permissions
 file { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet,
+  ensure  => file,
+  mode    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
