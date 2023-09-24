fc3abbd75d8747b4c41def4b4f546d06eb6f352f
H-Asmaa <asmaehadar32@gmail.com>
2023-09-24 18:28:23 +0100

    Committing a puppet code for ssh configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Puppet configuration script
 file_line { '/etc/ssh/ssh_config':
   ensure => present,
-  line => 'PasswordAuthentification no',
-  match => '^#?PasswordAuthentification'
-  mode => '0600'
+  line   => 'PasswordAuthentification no',
+  match  => '^#?PasswordAuthentification'
+  mode   => '0600'
 }
 file_line { '~/.ssh/school':
   ensure => present,
-  line => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile'
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile'
 }
