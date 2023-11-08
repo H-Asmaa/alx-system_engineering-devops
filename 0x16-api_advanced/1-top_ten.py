482e763576dc0090880f3ab8a67dbf57f098660c
H-Asmaa <asmaehadar32@gmail.com>
2023-11-08 07:18:26 +0100

    Committing a correction of output that must of been limited to 10 titles.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 def top_ten(subreddit):
     """A function function that queries the Reddit API and prints the
     titles of the first 10 hot posts listed for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(
         url, headers={"User-Agent": "custom"}, params={"Max-Results": 10}
     )
