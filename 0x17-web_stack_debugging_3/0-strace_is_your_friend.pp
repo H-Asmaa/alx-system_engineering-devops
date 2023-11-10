2e8a7dabe32efb161c5e7745040fe9cc97c6bed4
H-Asmaa <asmaehadar32@gmail.com>
2023-11-10 12:02:30 +0100

    Committing requirement fail fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # A script that removes p from php in the wp-settings.php file that was causing 500 status-code.
 exec { 'fixing-syntax':
-  command => "sed -i 's/.phpp/.php/g' '/var/www/html/wp-settings.php'",
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path    => ['/usr/bin', '/bin'],
 }
