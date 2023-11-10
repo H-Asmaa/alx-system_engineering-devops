4c9c582d1366bd2897d904af2540689512b59e3e
H-Asmaa <asmaehadar32@gmail.com>
2023-11-10 12:09:33 +0100

    Committing a correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # A script that removes p from php in the wp-settings.php file that was causing 500 status-code.
 $file_path = '/var/www/html/wp-settings.php'
-exec { 'fixing-syntax':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/usr/bin', '/bin'],
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+  path    => ['/bin','/usr/bin']
 }
